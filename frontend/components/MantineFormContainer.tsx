aca3759b41130b50ab0a5062f66e87dcfbb187d9
simutis <54304022+SimasG@users.noreply.github.com>
2022-12-13 14:07:57 +0200

    refactor offset functions

--- frontend/components/MantineFormContainer.tsx
+++ frontend/components/MantineFormContainer.tsx
@@ -1,7 +1,4 @@
 import { useAccount } from "wagmi";
-import addresses, { OHPolygonAddress } from "../constants/constants";
-import { OffsetHelperABI } from "../constants";
-import { ethers, FixedNumber } from "ethers"; // ** How am I using ethers without having it installed in my frontend here?
 
 import { Select, NumberInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
@@ -10,6 +7,7 @@ import { showNotification } from "@mantine/notifications";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import handleEstimate from "../utils/getEstimates";
+import offset from "../utils/offset";
 
 const MantineFormContainer = () => {
   const [carbonTokens, setCarbonTokens] = useState<
@@ -78,7 +76,6 @@ const MantineFormContainer = () => {
       } else {
         setEstimate("");
       }
-      // console.log("estimate:", estimate);
     };
     runHandleEstimate();
   }, [form.values]);
@@ -171,162 +168,19 @@ const MantineFormContainer = () => {
     }
   };
 
-  // * Blockchain-related functionality
-  // If paymentMethod = BCT/NCT -> autoOffsetPoolToken()
-  // If paymentMethod = MATIC & offsetMethod = BCT/NCT -> autoOffsetExactOutETH()
-  // If paymentMethod = MATIC & offsetMethod = MATIC -> autoOffsetExactInETH()
-  // If paymentMethod = WMATIC/USDC/WETH & offsetMethod = BCT/NCT -> autoOffsetExactOutToken()
-  // If paymentMethod = WMATIC/USDC/WETH & offsetMethod = WMATIC/USDC/WETH -> autoOffsetExactInToken()
-  const offset = (
-    paymentMethod: string,
-    offsetMethod: string,
-    amountToOffset: number
-  ) => {
-    if (paymentMethod === "bct" || paymentMethod === "nct") {
-      // ** Can't test it out atm
-      console.log("trigger autoOffsetPoolToken()");
-    } else if (paymentMethod === "matic") {
-      if (offsetMethod === "bct" || offsetMethod === "nct") {
-        // * Doesn't work
-        console.log("trigger autoOffsetExactOutETH()");
-        autoOffsetExactOutETH(offsetMethod, amountToOffset);
-      } else {
-        // * Works. Why?
-        console.log("trigger autoOffsetExactInETH()");
-        autoOffsetExactInETH(offsetMethod, amountToOffset);
-      }
-    } else {
-      if (offsetMethod === "bct" || offsetMethod === "nct") {
-        // * Doesn't work
-        console.log("trigger autoOffsetExactOutToken()");
-        autoOffsetExactOutToken(paymentMethod, offsetMethod, amountToOffset);
-      } else {
-        console.log("trigger autoOffsetExactInToken()");
-        // * Doesn't work
-        autoOffsetExactInToken(paymentMethod, offsetMethod, amountToOffset);
-      }
-    }
-  };
-
-  // ** `UNPREDICTABLE_GAS_LIMIT` if amountToOffset is > 0
-  // `UniswapV2Library: INSUFFICIENT_OUTPUT_AMOUNT` if amountToOffset is 0. Oke, I just can't have an input of 0, makes sense.
-  const autoOffsetExactOutToken = async (
-    paymentMethod: string,
-    offsetMethod: string,
-    amountToOffset: number
-  ) => {
-    const { ethereum } = window;
-    // @ts-ignore
-    const provider = new ethers.providers.Web3Provider(ethereum);
-    const signer = provider.getSigner();
-
-    const oh = new ethers.Contract(OHPolygonAddress, OffsetHelperABI, signer);
-
-    const depositedToken =
-      paymentMethod === "wmatic"
-        ? addresses.wmatic
-        : paymentMethod === "usdc"
-        ? addresses.usdc
-        : paymentMethod === "weth"
-        ? addresses.weth
-        : null;
-
-    const poolToken = offsetMethod === "bct" ? addresses.bct : addresses.nct;
-
-    const offsetTx = await oh.autoOffsetExactOutToken(
-      depositedToken,
-      poolToken,
-      FixedNumber.from(amountToOffset.toString())
-    );
-    await offsetTx.wait();
-    console.log("offset hash", offsetTx.hash);
-  };
-
-  // ** `UniswapV2Router: EXCESSIVE_INPUT_AMOUNT` if amountToOffset is > 0.
-  //  `UniswapV2Library: INSUFFICIENT_OUTPUT_AMOUNT` if amountToOffset is 0. Oke, I just can't have an input of 0, makes sense.
-  // Oke, now metamask tx is initiated but it says I'll need to pay 0 MATIC + gas. Doesn't make sense.
-  const autoOffsetExactOutETH = async (
-    offsetMethod: string,
-    amountToOffset: number
-  ) => {
-    const { ethereum } = window;
-    console.log("ethereum:", ethereum);
-
-    // This seems to be a more specific provider, built on "jsonRpc provider"
-    // @ts-ignore
-    const provider = new ethers.providers.Web3Provider(ethereum);
-    console.log("provider:", provider);
-
-    const signer = provider.getSigner();
-    console.log("signer:", signer);
-
-    const oh = new ethers.Contract(OHPolygonAddress, OffsetHelperABI, signer);
-    console.log("Offset Helper contract instance:", oh);
-
-    const poolToken = offsetMethod === "bct" ? addresses.bct : addresses.nct;
-
-    const offsetTx = await oh.autoOffsetExactOutETH(
-      poolToken,
-      ethers.utils.parseUnits(amountToOffset.toString(), "ether")
-    );
-    await offsetTx.wait();
-    console.log("offset hash", offsetTx.hash);
-  };
-
-  const autoOffsetExactInETH = async (
-    offsetMethod: string,
-    amountToOffset: number
-  ) => {
-    const { ethereum } = window;
-    // @ts-ignore
-    const provider = new ethers.providers.Web3Provider(ethereum);
-    const signer = provider.getSigner();
-
-    const oh = new ethers.Contract(OHPolygonAddress, OffsetHelperABI, signer);
-
-    const poolToken = offsetMethod === "bct" ? addresses.bct : addresses.nct;
-
-    const offsetTx = await oh.autoOffsetExactInETH(poolToken, {
-      value: ethers.utils.parseUnits(amountToOffset.toString(), "ether"),
-    });
-
-    await offsetTx.wait();
-    console.log("offset hash", offsetTx.hash);
-  };
-
-  // ** `UNPREDICTABLE_GAS_LIMIT` if amountToOffset is >= 0
-  const autoOffsetExactInToken = async (
-    paymentMethod: string,
-    offsetMethod: string,
-    amountToOffset: number
-  ) => {
-    const { ethereum } = window;
-    // @ts-ignore
-    const provider = new ethers.providers.Web3Provider(ethereum);
-    const signer = provider.getSigner();
-
-    const oh = new ethers.Contract(OHPolygonAddress, OffsetHelperABI, signer);
-
-    const poolToken = offsetMethod === "bct" ? addresses.bct : addresses.nct;
-
-    const offsetTx = await oh.autoOffsetExactInToken(
-      addresses[paymentMethod],
-      FixedNumber.from(amountToOffset.toString()),
-      poolToken
-    );
-    await offsetTx.wait();
-    console.log("offset hash", offsetTx.hash);
-  };
-
   const handleSubmit = async (values: typeof form.values) => {
     if (isConnected) {
       {
-        // offset(values.paymentMethod, values.offsetMethod, values.amountToOffset);
-        // toast.success(
-        //   `${
-        //     form.values.amountToOffset
-        //   } ${form.values.paymentMethod.toUpperCase()} has been offset!`
-        // );
+        offset(
+          values.paymentMethod,
+          values.offsetMethod,
+          values.amountToOffset
+        );
+        toast.success(
+          `${
+            form.values.amountToOffset
+          } ${form.values.paymentMethod.toUpperCase()} has been offset!`
+        );
       }
     } else {
       toast.error("Connect to a Wallet first!");
--- frontend/utils/offset.ts
+++ frontend/utils/offset.ts
@@ -1,5 +1,151 @@
-function xxx() {
-  return <div>xxx</div>;
-}
+import { ethers } from "ethers";
+import { OffsetHelperABI } from "../constants";
+import addresses, { OHPolygonAddress } from "../constants/constants";
+// * Blockchain-related functionality
+// If paymentMethod = BCT/NCT -> autoOffsetPoolToken()
+// If paymentMethod = MATIC & offsetMethod = BCT/NCT -> autoOffsetExactOutETH()
+// If paymentMethod = MATIC & offsetMethod = MATIC -> autoOffsetExactInETH()
+// If paymentMethod = WMATIC/USDC/WETH & offsetMethod = BCT/NCT -> autoOffsetExactOutToken()
 
-export default xxx;
+// If paymentMethod = WMATIC/USDC/WETH & offsetMethod = WMATIC/USDC/WETH -> autoOffsetExactInToken()
+const offset = (
+  paymentMethod: string,
+  offsetMethod: string,
+  amountToOffset: number
+) => {
+  if (paymentMethod === "bct" || paymentMethod === "nct") {
+    // ** Can't test it out atm
+    console.log("trigger autoOffsetPoolToken()");
+  } else if (paymentMethod === "matic") {
+    if (offsetMethod === "bct" || offsetMethod === "nct") {
+      // * Doesn't work
+      console.log("trigger autoOffsetExactOutETH()");
+      autoOffsetExactOutETH(offsetMethod, amountToOffset);
+    } else {
+      // * Works. Why?
+      console.log("trigger autoOffsetExactInETH()");
+      autoOffsetExactInETH(offsetMethod, amountToOffset);
+    }
+  } else {
+    if (offsetMethod === "bct" || offsetMethod === "nct") {
+      // * Doesn't work
+      console.log("trigger autoOffsetExactOutToken()");
+      autoOffsetExactOutToken(paymentMethod, offsetMethod, amountToOffset);
+    } else {
+      console.log("trigger autoOffsetExactInToken()");
+      // * Doesn't work
+      autoOffsetExactInToken(paymentMethod, offsetMethod, amountToOffset);
+    }
+  }
+};
+
+export default offset;
+
+// ** `UniswapV2Router: EXCESSIVE_INPUT_AMOUNT` if amountToOffset is > 0.
+//  `UniswapV2Library: INSUFFICIENT_OUTPUT_AMOUNT` if amountToOffset is 0. Oke, I just can't have an input of 0, makes sense.
+// Oke, now metamask tx is initiated but it says I'll need to pay 0 MATIC + gas. Doesn't make sense.
+const autoOffsetExactOutETH = async (
+  offsetMethod: string,
+  amountToOffset: number
+) => {
+  const { ethereum } = window;
+
+  // This seems to be a more specific provider, built on "jsonRpc provider"
+  // @ts-ignore
+  const provider = new ethers.providers.Web3Provider(ethereum);
+
+  const signer = provider.getSigner();
+
+  const oh = new ethers.Contract(OHPolygonAddress, OffsetHelperABI, signer);
+
+  const poolToken = offsetMethod === "bct" ? addresses.bct : addresses.nct;
+
+  const offsetTx = await oh.autoOffsetExactOutETH(
+    poolToken,
+    ethers.utils.parseEther(amountToOffset.toString())
+  );
+  await offsetTx.wait();
+  console.log("offset hash", offsetTx.hash);
+};
+
+const autoOffsetExactInETH = async (
+  offsetMethod: string,
+  amountToOffset: number
+) => {
+  const { ethereum } = window;
+  // @ts-ignore
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+
+  const oh = new ethers.Contract(OHPolygonAddress, OffsetHelperABI, signer);
+
+  const poolToken = offsetMethod === "bct" ? addresses.bct : addresses.nct;
+
+  const offsetTx = await oh.autoOffsetExactInETH(poolToken, {
+    value: ethers.utils.parseEther(amountToOffset.toString()),
+  });
+
+  await offsetTx.wait();
+  console.log("offset hash", offsetTx.hash);
+};
+
+// ** `UNPREDICTABLE_GAS_LIMIT` if amountToOffset is > 0
+// `UniswapV2Library: INSUFFICIENT_OUTPUT_AMOUNT` if amountToOffset is 0. Oke, I just can't have an input of 0, makes sense.
+const autoOffsetExactOutToken = async (
+  paymentMethod: string,
+  offsetMethod: string,
+  amountToOffset: number
+) => {
+  const { ethereum } = window;
+  // @ts-ignore
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+
+  const oh = new ethers.Contract(OHPolygonAddress, OffsetHelperABI, signer);
+
+  const depositedToken =
+    paymentMethod === "wmatic"
+      ? addresses.wmatic
+      : paymentMethod === "usdc"
+      ? addresses.usdc
+      : paymentMethod === "weth"
+      ? addresses.weth
+      : null;
+
+  const poolToken = offsetMethod === "bct" ? addresses.bct : addresses.nct;
+
+  const offsetTx = await oh.autoOffsetExactOutToken(
+    depositedToken,
+    poolToken,
+    ethers.utils.parseEther(amountToOffset.toString())
+
+    // FixedNumber.from(amountToOffset.toString())
+  );
+  await offsetTx.wait();
+  console.log("offset hash", offsetTx.hash);
+};
+
+// ** `UNPREDICTABLE_GAS_LIMIT` if amountToOffset is >= 0
+const autoOffsetExactInToken = async (
+  paymentMethod: string,
+  offsetMethod: string,
+  amountToOffset: number
+) => {
+  const { ethereum } = window;
+  // @ts-ignore
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+
+  const oh = new ethers.Contract(OHPolygonAddress, OffsetHelperABI, signer);
+
+  const poolToken = offsetMethod === "bct" ? addresses.bct : addresses.nct;
+
+  const offsetTx = await oh.autoOffsetExactInToken(
+    addresses[paymentMethod],
+    ethers.utils.parseEther(amountToOffset.toString()),
+    poolToken
+  );
+
+  await offsetTx.wait();
+  console.log("offset hash", offsetTx.hash);
+};
