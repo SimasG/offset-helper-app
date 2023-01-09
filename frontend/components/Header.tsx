f4eef7c7d1e85d5c24d8f8c2e21203f3203a7d7d
simutis <54304022+SimasG@users.noreply.github.com>
2023-01-09 18:49:31 +0100

    add calculate link

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,9 +1,17 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="flex items-center justify-end mb-12 sm:mb-24">
-      <ConnectButton />
+    <div className="flex items-center justify-between mb-12 sm:mb-24">
+      <ul>
+        <li className="text-base text-white transition duration-1000 hover:duration-1000 hover:underline hover:underline-offset-4">
+          <Link href="/calculate">Calculate Emissions</Link>
+        </li>
+      </ul>
+      <div>
+        <ConnectButton />
+      </div>
     </div>
   );
 };
---
+++ frontend/pages/calculate.tsx
@@ -0,0 +1,5 @@
+const calculate = () => {
+  return <div>calculate</div>;
+};
+
+export default calculate;
