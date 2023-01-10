6280595688c9a4a1bd365862c5831dbda16cb48f
simutis <54304022+SimasG@users.noreply.github.com>
2023-01-10 10:11:41 +0100

    progress with /calculate page responsiveness

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,22 +1,41 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Link from "next/link";
+import { useState } from "react";
 
 const Header = () => {
-  return (
-    <div className="flex items-center justify-between mb-12 sm:mb-24">
-      <ul className="flex items-center justify-between gap-6">
-        <li className="text-sm text-white transition duration-1000 hover:duration-1000 hover:underline hover:underline-offset-4">
-          <Link href="/">Home</Link>
-        </li>
-        <li className="text-sm text-white transition duration-1000 hover:duration-1000 hover:underline hover:underline-offset-4">
-          <Link href="/calculate">Calculate Emissions</Link>
-        </li>
-      </ul>
-      <div>
-        <ConnectButton />
+  const [openMenu, setOpenMenu] = useState(false);
+
+  console.log("openMenu:", openMenu);
+
+  {
+    return openMenu ? (
+      <div>Mobile Menu</div>
+    ) : (
+      <div className="flex items-center justify-between mb-12 sm:mb-24">
+        {/* Mobile */}
+        <button
+          className="block sm:hidden"
+          onClick={() => setOpenMenu(!openMenu)}
+        >
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </button>
+        {/* Tablet/Desktop */}
+        <ul className="items-center justify-between hidden gap-6 sm:flex">
+          <li className="text-sm text-white transition duration-1000 hover:duration-1000 hover:underline hover:underline-offset-4">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="text-sm text-white transition duration-1000 hover:duration-1000 hover:underline hover:underline-offset-4">
+            <Link href="/calculate">Calculate Emissions</Link>
+          </li>
+        </ul>
+        <div>
+          <ConnectButton />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Header;
--- frontend/pages/calculate.tsx
+++ frontend/pages/calculate.tsx
@@ -85,7 +85,7 @@ const calculate = () => {
             Carbon Footprint
           </mark>
         </h1>
-        <p className="mb-6 text-lg text-center text-gray-300 drop-shadow-2xl">
+        <p className="mb-8 text-lg text-center text-gray-300 drop-shadow-2xl">
           Easily estimate your emissions. Both on-chain and in real world.
         </p>
         {!openBlockchainCalculator && (
@@ -93,12 +93,12 @@ const calculate = () => {
             onClick={() => {
               setOpenBlockchainCalculator(!openBlockchainCalculator);
             }}
-            className="flex items-center justify-center gap-4"
+            className="flex flex-col items-center justify-center w-full gap-4 sm:flex-row"
           >
-            <button className="px-6 py-4 font-bold text-center text-white rounded-md bg-[#21B6A8] drop-shadow-2xl hover:opacity-75 transition-all">
+            <button className="w-5/6 sm:w-fit px-6 py-4 font-bold text-center text-white rounded-md bg-[#21B6A8] drop-shadow-2xl hover:opacity-75 transition-all">
               Blockchain Footprint
             </button>
-            <button className="px-6 py-4 font-bold text-center text-[#21B6A8] rounded-md drop-shadow-2xl bg-white hover:opacity-75 transition-all">
+            <button className="w-5/6 sm:w-fit px-6 py-4 font-bold text-center text-[#21B6A8] rounded-md drop-shadow-2xl bg-white hover:opacity-75 transition-all">
               Real World Footprint
             </button>
           </div>
